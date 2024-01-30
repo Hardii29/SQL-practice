51dad0afc5fed8f7f4149586dbcf505d186f16c9
hardii29 <hardi.jayani@etatvasoft.com>
2024-01-30 15:32:15 +0530

    exercise

--- postgre-3.sql
+++ postgre-3.sql
@@ -17,3 +17,18 @@ CREATE TABLE Details(
 roll_no SERIAL NOT NULL,
 	name varchar(50) not null
 );
+
+CREATE DATABASE mycommerce;
+
+CREATE TABLE order_details(
+	orderid SERIAL not null unique primary key,
+	customer_name varchar(50),
+	product_name varchar(20),
+	order_from varchar(10),
+	order_amount decimal(5,2),
+	order_date timestamp,
+	delivery_date timestamp
+);
+ALTER TABLE order_details RENAME COLUMN customer_name TO customer_first_name;
+
+ALTER TABLE order_details ADD COLUMN cancel_date timestamp;
